1b880f1531e6844f0da6884d0936ee8a060a89c8
Olli Eskola <olli.eskola@siili.com>
2019-02-26 18:53:49 +0200

    tehtävät 6.3 ja 6.4

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { addVote, createAnecdote } from './reducers/anecdoteReducer'
+
+const App = (props) => {
+  const anecdotes = props.store.getState()
+  
+  const vote = (id) => {
+    console.log('vote', id)
+    props.store.dispatch(
+      addVote(id)
+    )
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    props.store.dispatch(
+      createAnecdote(event.target.newAnecdote.value)
+    )
+    event.target.newAnecdote.value = ''
+  }
+  
+  return (
+    
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="newAnecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
